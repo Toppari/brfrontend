cbe9686f37f19301105ef8162b8b16848853f001
Toppari <oksanen.topias@gmail.com>
2019-03-07 16:29:36 +0100

    fetching and displaying some data from battlerite api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import useBattleriteAPI from './api/useBattleriteAPI';
 
 const App = () => {
+  const [query, setQuery] = useState('');
+  const [data, isLoading, error, getData] = useBattleriteAPI();
+
+  const onChange = event => {
+    setQuery(event.target.value);
+  };
+
+  const onClick = () => {
+    if (query.length) {
+      getData(query);
+      setQuery('');
+    }
+  };
+
   return (
+    <div>
+      <h1>Search player</h1>
+
+      <input id="player_name" type="text" value={query} onChange={onChange} placeholder="Player name"/>
+      <button type="button" onClick={onClick}>Submit</button>
+
       <div>
-        <h4>Battlerite frontend</h4>
+        {isLoading ? <h5>Loading...</h5> : null}
+        {error ? <h5>{error}</h5> : <h5>{data}</h5>}
       </div>
+    </div>
   );
 };
 
---
+++ src/api/useBattleriteAPI.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default () => {
+  const [data, setData] = useState('');
+  const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const fetchData = async () => {
+    setError('');
+    setIsLoading(true);
+
+    try {
+      const response = await axios.get(query, {
+        headers: {
+          Authorization: 'Bearer ' + process.env.REACT_APP_BR_API_KEY,
+          Accept: 'application/json'
+        }
+      });
+      setData(response.data.data[0].attributes.name);
+    } catch (error) {
+      setError(error.response.data.errors[0].title);
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (query) {
+      fetchData();
+    }
+  }, [query]);
+
+  const getData = query => {
+    setQuery(`https://api.developer.battlerite.com/shards/global/players?filter[playerNames]=${query}`);
+  };
+
+  return [data, isLoading, error, getData];
+};
