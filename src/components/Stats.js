813810975336b4e261a3abf6c261cdd716ac8511
Toppari <oksanen.topias@gmail.com>
2019-03-26 12:29:08 +0100

    require icon images

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -2,17 +2,11 @@ import React from 'react';
 import { Statistic, Image } from 'semantic-ui-react';
 
 const Stats = ({ name, value, iconHash }) => {
-  const icon = () => {
-    try {
-      return require(`../assets/${iconHash}.tga`);
-    } catch (error) {
-      return require(`../assets/${iconHash}.png`);
-    }
-  };
-
   return (
     <React.Fragment>
-      {iconHash && <Image src={icon()} circular />}
+      {iconHash && (
+        <Image src={require(`../assets/${iconHash}.png`)} circular />
+      )}
       <Statistic horizontal label={name} value={value} />
     </React.Fragment>
   );
